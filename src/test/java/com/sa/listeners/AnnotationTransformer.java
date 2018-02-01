4435dd8320c3927ba54ffa96db79fdd51886aa88
Manikanta451 <mani6747@gmail.com>
2018-02-01 12:24:16 +0530

    Listener code is updated

--- src/test/java/com/sa/listeners/AnnotationTransformer.java
+++ src/test/java/com/sa/listeners/AnnotationTransformer.java
@@ -4,12 +4,17 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+
 import com.sa.testscripts.SetupEnvironment;
+
+import org.testng.Assert;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
 
 public class AnnotationTransformer implements IAnnotationTransformer {
+	
+	public String excep;
 	
 	@SuppressWarnings({ "rawtypes"})
 	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod){ 
@@ -21,19 +26,29 @@ public class AnnotationTransformer implements IAnnotationTransformer {
 				ae = se.start();
     	}
     	catch (IllegalAccessException e) {
-				e.printStackTrace();
+    		excep=e.toString();
+	    	Assert.fail(excep);
+			e.printStackTrace();
 		}
-    	catch (IllegalArgumentException e) {		
-				e.printStackTrace();
+    	catch (IllegalArgumentException e) {
+    		excep=e.toString();
+	    	Assert.fail(excep);
+			e.printStackTrace();
 		}
-    	catch (InvocationTargetException e) {    	
-				e.printStackTrace();
+    	catch (InvocationTargetException e) {
+    		excep=e.toString();
+	    	Assert.fail(excep);
+			e.printStackTrace();
 		}
     	catch (NoSuchMethodException e) {
-				e.printStackTrace();
+    		excep=e.toString();
+	    	Assert.fail(excep);
+			e.printStackTrace();
 		}
     	catch (SecurityException e) {
-				e.printStackTrace();
+    		excep=e.toString();
+	    	Assert.fail(excep);
+			e.printStackTrace();
 		}	
 		if (ae.contains(annotation.getTestName())) 
 		{
