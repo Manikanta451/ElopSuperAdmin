738da1a8ae098a706d19470f7e300b5e28b8f281
Manikanta451 <mani6747@gmail.com>
2018-03-12 17:00:25 +0530

    Latest

--- src/test/java/com/sa/pagefactory/Parents.java
+++ src/test/java/com/sa/pagefactory/Parents.java
@@ -17,9 +17,9 @@ public class Parents extends ParentsPageObjects {
 	
 	public void parentslist()throws Exception {
 		
-		        waitForSeconds(5);
+		        waitForSeconds(3);
 		        Parents.click();
-		        waitForSeconds(8);
+		        waitForSeconds(1);
 		        webtablehandle();
 		       /* EditParent.click();
 		        waitForSeconds(5);
@@ -34,27 +34,40 @@ public class Parents extends ParentsPageObjects {
 
 	public void webtablehandle()throws Exception{
 		
-				WebElement mytable = driver.findElement(By.id("Sample_parentlist"));
-				List < WebElement > rows_table = mytable.findElements(By.tagName("tr"));
-				int rows_count = rows_table.size();
-				for (int row = 0; row < rows_count; row++) {
-				List < WebElement > Columns_row = rows_table.get(row).findElements(By.tagName("td"));
-    	        int columns_count = Columns_row.size();
-    	        //System.out.println("Number of cells In Row " + row + " are " + columns_count);
-    	        for (int column = 0; column < columns_count; column++) {
-    	       // String celtext = Columns_row.get(column).getText();
-    	        //System.out.println("Cell Value of row number " + row + " and column number " + column + " Is " + celtext);
+		boolean breakIt = true;
+        while (true) {
+        breakIt = true;
+        try {
+        	WebElement mytable = driver.findElement(By.id("Sample_parentlist"));
+			List < WebElement > rows_table = mytable.findElements(By.tagName("tr"));
+			int rows_count = rows_table.size();
+			for (int row = 0; row < rows_count; row++) {
+			List < WebElement > Columns_row = rows_table.get(row).findElements(By.tagName("td"));
+	        int columns_count = Columns_row.size();
+	        //System.out.println("Number of cells In Row " + row + " are " + columns_count);
+	        for (int column = 0; column < columns_count; column++) {
+	       // String celtext = Columns_row.get(column).getText();
+	        //System.out.println("Cell Value of row number " + row + " and column number " + column + " Is " + celtext);
+	        	if(row==2 && column==7){
+    	        	EditParent.click();
     	        	
     	        }
-    	        if(row==1 && columns_count==7){
-    	        	EditParent.click();
-    	        }
-    	}
-	}
+	        }
+	        
 	
+			}
+
+
+        } catch (Exception e) {
+            if (e.getMessage().contains("element is not attached")) {
+                breakIt = false;
+            }
+        }
+        if (breakIt) {
+            break;
+        }
+
+    }	
 	
-	
-	
-	
-	
+  }
 }
