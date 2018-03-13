a50389d3068fffe0e4f429ea45ad9b691e645316
Manikanta451 <mani6747@gmail.com>
2018-03-13 20:57:33 +0530

    Handle webtable

--- src/test/java/com/sa/pagefactory/Parents.java
+++ src/test/java/com/sa/pagefactory/Parents.java
@@ -1,5 +1,6 @@
 package com.sa.pagefactory;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -19,7 +20,7 @@ public class Parents extends ParentsPageObjects {
 		
 		        waitForSeconds(3);
 		        Parents.click();
-		        waitForSeconds(1);
+		        waitForSeconds(8);
 		        webtablehandle();
 		       /* EditParent.click();
 		        waitForSeconds(5);
@@ -32,42 +33,45 @@ public class Parents extends ParentsPageObjects {
 		        
 	}
 
-	public void webtablehandle()throws Exception{
-		
-		boolean breakIt = true;
-        while (true) {
-        breakIt = true;
-        try {
-        	WebElement mytable = driver.findElement(By.id("Sample_parentlist"));
-			List < WebElement > rows_table = mytable.findElements(By.tagName("tr"));
-			int rows_count = rows_table.size();
-			for (int row = 0; row < rows_count; row++) {
-			List < WebElement > Columns_row = rows_table.get(row).findElements(By.tagName("td"));
-	        int columns_count = Columns_row.size();
-	        //System.out.println("Number of cells In Row " + row + " are " + columns_count);
-	        for (int column = 0; column < columns_count; column++) {
-	       // String celtext = Columns_row.get(column).getText();
-	        //System.out.println("Cell Value of row number " + row + " and column number " + column + " Is " + celtext);
-	        	if(row==2 && column==7){
-    	        	EditParent.click();
-    	        	
-    	        }
-	        }
-	        
 	
-			}
 
+    public void webtablehandle()throws Exception{
 
-        } catch (Exception e) {
-            if (e.getMessage().contains("element is not attached")) {
-                breakIt = false;
-            }
-        }
-        if (breakIt) {
-            break;
-        }
-
+				boolean breakIt = true;
+				while (true) {
+				breakIt = true;
+				try {
+				WebElement mytable = driver.findElement(By.id("Sample_parentlist"));
+				List < WebElement > rows_table = mytable.findElements(By.tagName("tr"));
+				int rows_count = rows_table.size();
+				for (int row =0; row < rows_count; row++) {
+				List < WebElement > Columns_row = rows_table.get(row).findElements(By.tagName("td"));
+				int columns_count = Columns_row.size();
+				//System.out.println("Number of cells In Row " + row + " are " + columns_count);
+					for (int column = 0; column < columns_count; column++) {
+			    /*String celtext = Columns_row.get(column).getText();
+			    System.out.println("Cell Value of row number " + row + " and column number " + column + " Is " + celtext);*/
+			    	if (row == 5 && column == 7) {
+			   	List < WebElement > tags_row = Columns_row.get(7).findElements(By.tagName("a"));
+			   	Iterator<WebElement> iterator = tags_row.iterator();
+			   	  	while (iterator.hasNext()) {
+			   	  WebElement next = iterator.next();
+			   	  if(next.getAttribute("href").contains("Edit")) {
+			   	  next.click();
+			                  break;
+	          		              }
+			   	  			}
+			    		}
+			    	}
+                 }
+      } catch (Exception e) {
+          if (e.getMessage().contains("element is not attached")) {
+              breakIt = false;
+          }
+      }
+      if (breakIt) {
+          break;
+      }
     }	
-	
   }
 }
